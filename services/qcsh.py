5bfc429da1420d408d6973214695e3028e99c0fd
vvbbnn00 <vvbbnn00@foxmail.com>
2024-01-11 20:50:26 +0800

    新增学习完成截图功能 - 新增获取学习完成截图功能 - 消息推送包含学习完成截图 - 支持设置是否自动保存学习完成截图 - 自动学习时间修正为东八区11点30分

--- services/qcsh.py
+++ services/qcsh.py
@@ -10,6 +10,7 @@ import json
 import logging
 import os
 import re
+import time
 
 import requests
 
@@ -19,6 +20,9 @@ from urllib.parse import urljoin
 
 
 def getChildrenNode(pid):
+    """
+    获取子节点
+    """
     ret = requests.get(QCSH_AREA_LIST_URL.format(pid=pid), headers={
         'Users-Agent': USER_AGENT
     }, proxies=PROXY)
@@ -36,6 +40,10 @@ class QcshService:
             PROXY = proxy
 
     def login(self, login_data):
+        """
+        通过LoginData登录青年大学习
+        :param login_data: 登录信息
+        """
         open_id = login_data.get("openid")
         nickname = login_data.get("nickname")
         avatar = login_data.get("headimg")
@@ -50,34 +58,50 @@ class QcshService:
         return self.accessToken
 
     def getLatestInfo(self):
+        """
+        获取最新的学习ID
+        """
         ret = requests.get(QCSH_STUDY_INFO_URL.format(access_token=self.accessToken), headers={
             'Users-Agent': USER_AGENT
         }, proxies=PROXY)
         ret_json = ret.json()
         return ret_json.get("result")[0].get("id")
-    
-    def getLatestURL(self):
+
+    def getLatestEndPicURL(self):
+        """
+        获取最新的学习完成截图URL
+        """
         ret = requests.get(QCSH_STUDY_INFO_URL.format(access_token=self.accessToken), headers={
             'Users-Agent': USER_AGENT
         }, proxies=PROXY)
         ret_json = ret.json()
-        return ret_json.get("result")[0].get("uri")
-    
+        url = ret_json.get("result")[0].get("uri")
+        if url is None:
+            return None
+        return urljoin(url, "images/end.jpg")
+
     def downloadEndPic(self):
-        base_url = self.getLatestURL()
-        local_path = "qndxximg/endimg.jpg"
-        pic_url = urljoin(base_url,"images/end.jpg")
+        """
+        下载学习完成截图
+        """
+        pic_url = self.getLatestEndPicURL()
+        if pic_url is None:
+            return False, '未找到截图URL'
+        timestamp = int(time.time() * 1000)
+        local_path = f"qndxximg/endimg_{timestamp}.jpg"
         response = requests.get(pic_url)
         if response.status_code == 200:
             os.makedirs(os.path.dirname(local_path), exist_ok=True)
             with open(local_path, "wb") as f:
                 f.write(response.content)
-            return True,local_path
+            return True, local_path
         else:
-            return False,pic_url
-
+            return False, pic_url
 
     def updateStudyRecord(self, pid, student_number, subOrg=None):
+        """
+        更新学习记录
+        """
         ret = requests.post(QCSH_STUDY_URL.format(access_token=self.accessToken), headers={
             'Users-Agent': USER_AGENT
         }, json={
@@ -92,6 +116,9 @@ class QcshService:
         return False, json.dumps(ret_json, ensure_ascii=False)
 
     def getLastStudyInfo(self):
+        """
+        获取最后一次学习记录
+        """
         ret = requests.get(QCSH_STUDY_LAST_INFO_URL.format(access_token=self.accessToken), headers={
             'Users-Agent': USER_AGENT
         }, proxies=PROXY)
