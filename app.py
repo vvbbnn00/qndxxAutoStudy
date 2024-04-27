00bbcd45f375af3eadee3d82745925c290f424af
liuweiqing <liuweiqing147@gmail.com>
2024-04-27 09:28:33 +0800

    fix:无法https访问

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ from services.qcsh import getChildrenNode, QcshService
 
 app = Flask(__name__)
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 @app.route('/')
 def index():
